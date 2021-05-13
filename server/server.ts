751cf759f3e9b52b3bf56df7b5207d75aed5f72f
shovalp <shovalper@gmail.com>
2021-05-13 08:34:10 +0300

    init commit

---
+++ server/server.ts
@@ -0,0 +1,14 @@
+import express = require('express');
+
+// Create a new express app instance
+const app: express.Application = express();
+app.use(express.json());
+app.use(express.urlencoded());
+app.post('/remove', function (req, res) {
+  console.log('req: ', req.body);
+  res.send(req.body.driver);
+});
+app.listen(3000, function () {
+  console.log('App is listening on port 3000!'
+  );
+});
